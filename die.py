e753a51db662ed753252bb0ddb633d8a15fd9e5e
isaac <isaaccraciun13@gmail.com>
2022-07-17 21:31:35 +0100

    added feature - added feature to die - made the age finder program cleaner

--- die.py
+++ die.py
@@ -2,10 +2,14 @@ import random
 
 list = ["one", "two", "three", "four", "five", "six"]			# List
 list2 = ["heads", "tails"]
+list3 = ["red", "orange", "yellow", "green", "blue", "indigo", "purple", "pink", "white","black","grey"]
 item2 = random.choice(list2)
 item = random.choice(list)						# Chooses from list
-dice = float(input("to roll dice, press 2, to flip coin, press 1 "))
+item3 = random.choice(list3)
+dice = float(input("to roll dice, press 1, to flip coin, press 2, to pick a random colour press 3 \n "))
 if dice == 1 :
   print (item)
 elif dice == 2 :
   print (item2)
+elif dice == 3 :
+  print (item3)
